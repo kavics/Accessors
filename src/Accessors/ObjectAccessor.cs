19ad8599c1d3f3244da4b0b4c708dd2457d1891d
Zoltan Gyebrovszki <gyeby5@gmail.com>
2019-08-24 16:20:07 +0200

    Field and property accessor implementations.

--- src/Accessors/ObjectAccessor.cs
+++ src/Accessors/ObjectAccessor.cs
@@ -35,29 +35,76 @@ namespace Accessors
 
         public object GetField(string fieldName)
         {
-            throw new NotImplementedException();
+            var field = GetFieldInfo(fieldName);
+            return field.GetValue(Target);
         }
         public void SetField(string fieldName, object value)
         {
-            throw new NotImplementedException();
+            var field = GetFieldInfo(fieldName);
+            field.SetValue(Target, value);
+        }
+        private FieldInfo GetFieldInfo(string name, bool throwOnError = true)
+        {
+            var field = _targetType.GetField(name, _publicFlags) ?? _targetType.GetField(name, _privateFlags);
+            if (field == null && throwOnError)
+                throw new ApplicationException("Field not found: " + name);
+            return field;
         }
 
         public object GetProperty(string propertyName)
         {
-            throw new NotImplementedException();
+            var property = GetPropertyInfo(propertyName);
+            var method = property.GetGetMethod(true) ?? property.GetGetMethod(false);
+            return method.Invoke(Target, null);
         }
         public void SetProperty(string propertyName, object value)
         {
-            throw new NotImplementedException();
+            var property = GetPropertyInfo(propertyName);
+            var method = property.GetSetMethod(true) ?? property.GetSetMethod(false);
+            method.Invoke(Target, new object[] { value });
+        }
+        private PropertyInfo GetPropertyInfo(string name, bool throwOnError = true)
+        {
+            var property = _targetType.GetProperty(name, _publicFlags) ?? _targetType.GetProperty(name, _privateFlags);
+            if (property == null && throwOnError)
+                throw new ApplicationException("Property not found: " + name);
+            return property;
         }
 
         public object GetFieldOrProperty(string memberName)
         {
-            throw new NotImplementedException();
+            var field = GetFieldInfo(memberName, false);
+            if (field != null)
+                return field.GetValue(Target);
+
+            var property = GetPropertyInfo(memberName, false);
+            if (property == null)
+                throw new ApplicationException("Field or property not found: " + memberName);
+
+            var method = property.GetGetMethod(true) ?? property.GetGetMethod(false);
+            if (method == null)
+                throw new ApplicationException("The property does not have getter: " + memberName);
+
+            return method.Invoke(Target, null);
         }
         public void SetFieldOrProperty(string memberName, object value)
         {
-            throw new NotImplementedException();
+            var field = GetFieldInfo(memberName, false);
+            if (field != null)
+            {
+                field.SetValue(Target, value);
+                return;
+            }
+
+            var property = GetPropertyInfo(memberName, false);
+            if (property == null)
+                throw new ApplicationException("Field or property not found: " + memberName);
+
+            var method = property.GetSetMethod(true) ?? property.GetSetMethod(false);
+            if (method == null)
+                throw new ApplicationException("The property does not have setter: " + memberName);
+
+            method.Invoke(Target, new object[] { value });
         }
 
         public object Invoke(string name, params object[] args)
